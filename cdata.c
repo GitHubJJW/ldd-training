f0a7b63e530fad2bb1e54bf87076bcd9c00679dd
scott <jjwmessage@gmail.com>
2012-02-11 18:14:32 -0800

    add get minor number

--- cdata.c
+++ cdata.c
@@ -19,28 +19,38 @@
 
 #define DEV_MAJOR 121
 #define DEV_NAME  "debug"
-
+//system call for open
 static int cdata_open(struct inode *inode, struct file *filp)
 {
+	if(MINOR(inode->i_rdev) != 0){
+		printk("error minor number");
+		return -ENODEV;
+	}
 	printk(KERN_INFO "CDATA: open a device.\n");
+	printk(KERN_INFO "minor number = %d", MINOR(inode->i_rdev));
 	return 0;
 }
-
+//system call for close
 static int cdata_close(struct inode *inode, struct file *filp)
 {
 	printk(KERN_INFO "CDATA: close a device.\n");
 	return 0;
 }
-
+//system call for write
 ssize_t cdata_write(struct file *filp, const char *buf, size_t size, loff_t *off)
 {
 	return 0;
 }
 
+//define file operations
 static struct file_operations cdata_fops = {	
+	owner:		THIS_MODULE,
 	open:		cdata_open,
 	release:	cdata_close,
+	//read:		cdata_read,
 	write:		cdata_write,
+	//ioctl:		cdata_ioctl,
+	
 };
 
 
@@ -48,7 +58,7 @@ int cdata_init_module(void)
 {
 	MSG("CDATA V0.1.0");
 	MSG("  Copyright (C) 2004 www.jollen.org");
-	
+	//register driver (VFS)
 	if(register_chrdev(121, "cdata", &cdata_fops) < 0){
 		printk(KERN_INFO "CDATA: couldn't register a device.\n");
 		return -1;
@@ -57,6 +67,7 @@ int cdata_init_module(void)
 
 void cdata_cleanup_module(void)
 {
+	//unregister driver (VFS)
 	unregister_chrdev(DEV_MAJOR, "cdata");
 }
 
